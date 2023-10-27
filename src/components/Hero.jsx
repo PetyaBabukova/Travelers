856405ccb4805ed72778cf28d8112661f8f722d0
Petya Babukova <p_babukova@abv.bg>
2023-10-27 16:59:43 +0300

    Set the inotial design

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,39 +1,64 @@
 export default function Hero() {
     return (
         <div>
-            {/* Topbar Start */}
-            <div className="container-fluid bg-light pt-3 d-none d-lg-block">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-6 text-center text-lg-left mb-2 mb-lg-0">
-                            <div className="d-inline-flex align-items-center">
-                                <p><i className="fa fa-envelope mr-2"></i>info@example.com</p>
-                                <p className="text-body px-3">|</p>
-                                <p><i className="fa fa-phone-alt mr-2"></i>+012 345 6789</p>
-                            </div>
-                        </div>
-                        <div className="col-lg-6 text-center text-lg-right">
-                            <div className="d-inline-flex align-items-center">
-                                <a className="text-primary px-3" href="">
-                                    <i className="fab fa-facebook-f"></i>
-                                </a>
-                                <a className="text-primary px-3" href="">
-                                    <i className="fab fa-twitter"></i>
-                                </a>
-                                <a className="text-primary px-3" href="">
-                                    <i className="fab fa-linkedin-in"></i>
-                                </a>
-                                <a className="text-primary px-3" href="">
-                                    <i className="fab fa-instagram"></i>
-                                </a>
-                                <a className="text-primary pl-3" href="">
-                                    <i className="fab fa-youtube"></i>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  {/* Carousel Start */}
+  <div className="container-fluid p-0">
+    <div id="header-carousel" className="carousel slide" data-ride="carousel">
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img className="w-100" src="img/carousel-1.jpg" alt="Image" />
+          <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
+            <div className="p-3" style={{ maxWidth: 900 }}>
+              <h4 className="text-white text-uppercase mb-md-3">
+                Tours &amp; Travel
+              </h4>
+              <h1 className="display-3 text-white mb-md-4">
+                Let's Discover The World Together
+              </h1>
+              <a href="" className="btn btn-primary py-md-3 px-md-5 mt-2">
+                Book Now
+              </a>
             </div>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <img className="w-100" src="img/carousel-2.jpg" alt="Image" />
+          <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
+            <div className="p-3" style={{ maxWidth: 900 }}>
+              <h4 className="text-white text-uppercase mb-md-3">
+                Tours &amp; Travel
+              </h4>
+              <h1 className="display-3 text-white mb-md-4">
+                Discover Amazing Places With Us
+              </h1>
+              <a href="" className="btn btn-primary py-md-3 px-md-5 mt-2">
+                Book Now
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a
+        className="carousel-control-prev"
+        href="#header-carousel"
+        data-slide="prev"
+      >
+        <div className="btn btn-dark" style={{ width: 45, height: 45 }}>
+          <span className="carousel-control-prev-icon mb-n2" />
+        </div>
+      </a>
+      <a
+        className="carousel-control-next"
+        href="#header-carousel"
+        data-slide="next"
+      >
+        <div className="btn btn-dark" style={{ width: 45, height: 45 }}>
+          <span className="carousel-control-next-icon mb-n2" />
+        </div>
+      </a>
+    </div>
+  </div>
+  {/* Carousel End */}
             {/* Topbar End */}
 
         </div>
